a7e8540f9a6b90ea96d0a8900a4c9e942a331e5b
Christian <ferchris82@hotmail.com>
2024-01-17 15:16:53 -0500

    Versión 1.0

--- src/main/java/org/chrisferdev/java/jdbc/EjemploJdbc.java
+++ src/main/java/org/chrisferdev/java/jdbc/EjemploJdbc.java
@@ -1,26 +1,27 @@
 package org.chrisferdev.java.jdbc;
 
+import org.chrisferdev.java.jdbc.util.ConexionBaseDatos;
+
 import java.sql.*;
 
 public class EjemploJdbc {
     public static void main(String[] args) {
 
-        String url = "jdbc:mysql://localhost:3306/java_curso?serverTimezone=UTC";
-        String username = "root";
-        String password = "root";
-        try {
-            Connection conn = DriverManager.getConnection(url, username, password);
-            Statement stmt = conn.createStatement();
-            ResultSet resultado = stmt.executeQuery("SELECT * FROM productos");
+        try (Connection conn = ConexionBaseDatos.getInstance();
+             Statement stmt = conn.createStatement();
+             ResultSet resultado = stmt.executeQuery("SELECT * FROM productos")){
 
             while(resultado.next()){
-                System.out.println(resultado.getString("nombre"));
+                System.out.print(resultado.getInt("id"));
+                System.out.print(" | ");
+                System.out.print(resultado.getString("nombre"));
+                System.out.print(" | ");
+                System.out.print(resultado.getInt("precio"));
+                System.out.print(" | ");
+                System.out.println(resultado.getDate("fecha_registro"));
             }
-            resultado.close();
-            stmt.close();
-            conn.close();
-        } catch (SQLException throwables) {
-            throwables.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
     }
 }
